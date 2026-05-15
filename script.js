31a850233f5aeca1fd469221f8c82f54a2ebc6c1
caue85101 <caue85101@gmail.com>
2026-05-14 23:16:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("cadastroBtn").addEventListener("click", function() {
+  document.body.innerHTML = `
+    <div style="display:flex; justify-content:center; align-items:center; height:100vh; font-family:Arial; background:#f4d7a8;">
+      <h1>Aqui era pra ser a parte do ChatBot, porém o 2º ano nao mandou o link. :(</h1>
+    </div>
+  `;
+});
